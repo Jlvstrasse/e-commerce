3771408964e39b025c722a1f4f5588ca6659cd3e
Jlvstrasse <jlvstrasse@gmail.com>
2024-06-29 09:55:09 -0400

    pool variable

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
-
+const { Pool } = require('pg');
 const Sequelize = require('sequelize');
+
+const pool = new Pool(
+  {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    database: process.env.DB_NAME
+  },
+  console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
+
+pool.connect();
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
@@ -11,5 +23,5 @@ const sequelize = process.env.DB_URL
         decimalNumbers: true,
       },
     });
-
-module.exports = sequelize;
+    
+module.exports = sequelize, pool;
